39937f5fe6589001c5961bd00a2a27ca2bc4d1c3
Your Name <esmael@localhost.localdomain>
2024-02-07 01:28:49 +0200

    fix and new

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+"""Defines a Python class-to-JSON function."""
 
 
-import json
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
 
-
-def load_from_json_file(filename):
-    with open(filename, "r", encoding="UTF-8") as f:
-        return json.load(f)
