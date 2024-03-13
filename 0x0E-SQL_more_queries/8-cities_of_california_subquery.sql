26a58315f313e79f771e018a6e7453550d2ce195
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-13 16:13:33 -0700

    task 8 fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,12 @@
-SELECT cities.id, cities.name
-FROM cities, states
-WHERE cities.state_id = states.id
-AND states.name = 'California'
-ORDER BY cities.id ASC;
+-- Select the id and name of cities
+SELECT id, name
+-- Filter the cities where the state_id matches the id of the state named "California"
+FROM cities
+WHERE state_id = (
+      -- Subquery to find the id of the state named "California"
+      SELECT id
+      FROM states
+      WHERE name = "California")
+-- Order the results by the city id in ascending order
+ORDER BY id;
 
