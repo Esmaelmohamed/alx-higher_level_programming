2d3093dd2a299c655222622dfafa9bd2e2449dcf
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-12 17:56:02 -0700

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- Script to list all records of the table second_table ordered by score
+
+-- Select the score and name fields from second_table, ordered by score
+USE `hbtn_0c_0`;
+SELECT score, name FROM second_table ORDER BY score DESC;
+
