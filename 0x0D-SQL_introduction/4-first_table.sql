742355b7073b742568fe50426c3cc080c32ef44d
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-12 17:44:11 -0700

    task4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Script to create the table first_table if it doesn't exist
+
+-- Check if the table exists, and if not, create it
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
