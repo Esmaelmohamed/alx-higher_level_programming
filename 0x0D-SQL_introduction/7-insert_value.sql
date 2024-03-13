3bdad3c7a6fa1177608658f86107c1e96765dc89
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-12 17:52:10 -0700

    task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- Script to insert a new row into the table first_table
+
+-- Insert the new row into the first_table
+USE `hbtn_0c_0`;
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+
