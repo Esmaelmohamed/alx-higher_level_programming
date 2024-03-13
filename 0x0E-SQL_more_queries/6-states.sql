dedbeedb2c1aec752ecd53229c0aacb57e422169
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-13 16:09:37 -0700

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the created database
+USE hbtn_0d_usa;
+
+-- Create table if not exists
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
