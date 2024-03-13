b10a895d3d12abe1c1c76d4658ab936a0084666a
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-13 16:16:03 -0700

    9 fix code

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,12 +1,9 @@
--- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
--- Select the city ID, city name, and state name
-SELECT cities.id, cities.name, states.name AS state_name
+-- Select the city id, city name, and state name
+SELECT cities.id, cities.name, states.name
 -- Join the cities table with the states table based on the state_id column
 -- to get the corresponding state name for each city
 FROM cities
 JOIN states ON cities.state_id = states.id
--- Order the results by the city ID in ascending order
-ORDER BY cities.id ASC;
+-- Order the results by the city id in ascending order
+ORDER BY cities.id;
 
