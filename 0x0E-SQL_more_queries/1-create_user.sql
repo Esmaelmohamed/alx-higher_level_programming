9a5a9ddb0478b7503977f771881d239a9267904d
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-13 16:00:07 -0700

    task2

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create user 'user_0d_1' if it doesn't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user 'user_0d_1' on all databases and tables
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+
+-- Refresh the privileges to apply the changes
+FLUSH PRIVILEGES;
+
