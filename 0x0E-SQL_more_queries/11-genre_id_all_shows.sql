dd12217b57cdb8f31e6224dcdd1fdcf3d8aacfb8
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-13 16:18:20 -0700

    11

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,9 @@
+-- Select the title of TV shows and their corresponding genre IDs
+SELECT tv_shows.title, tv_show_genres.genre_id
+-- Perform a LEFT JOIN between the tv_shows table and the tv_show_genres table
+-- based on the show_id column to get the genre IDs associated with each TV show
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+-- Order the results first by the title of the TV show and then by the genre ID
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
+
