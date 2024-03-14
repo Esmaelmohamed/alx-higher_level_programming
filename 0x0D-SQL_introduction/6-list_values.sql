135836b661a7201d1e118047db990293c60df185
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:52:23 -0700

    task 6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,6 +1,3 @@
--- Script to list all rows of the table first_table from a specified database
-
--- Select all fields from the first_table
-USE `hbtn_0c_0`;
-SELECT * FROM first_table;
+-- Retrieve all rows and columns from the specified table
+SELECT * FROM `first_table`;
 
