50db5622b7dd1bb2e7eb357e77094d2ca676e788
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:55:23 -0700

    8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,6 +1,5 @@
--- Script to count the number of records with id = 89 in the table first_table
-
--- Select the count of records with id = 89
-USE `hbtn_0c_0`;
-SELECT COUNT(*) FROM first_table WHERE id = 89;
+-- Count the number of rows where id is 89
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
 
