bd1fd3d0adf1002bc866f5546ab52b0d176934ed
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:56:41 -0700

    fix 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
--- Script to create the table second_table and insert multiple rows
-
--- Create the table second_table if it doesn't exist
-USE `hbtn_0c_0`;
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
+-- Create the table `second_table` if it doesn't exist
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
 );
 
--- Insert multiple rows into second_table
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+-- Insert rows into the `second_table`
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
 
