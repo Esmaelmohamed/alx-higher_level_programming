d6dfca06857487829352720928f1f77256c5e95a
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:49:33 -0700

    sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,14 +1,2 @@
-DELIMITER //
-
-CREATE PROCEDURE list_tables_in_database(IN database_name VARCHAR(255))
-BEGIN
-    SET @query = CONCAT('USE ', database_name, ';');
-    PREPARE statement FROM @query;
-    EXECUTE statement;
-    DEALLOCATE PREPARE statement;
-
-    SHOW TABLES;
-END //
-
-DELIMITER ;
-
+-- show tabels 
+SHOW TABLES;
