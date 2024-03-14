9489296ad15dbd15e7da6d129ae94233cf47e549
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 12:06:42 -0700

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,9 +1,6 @@
--- Script to list all records of the table second_table with a name value in descending score order
-
--- Select the score and name fields from second_table where name is not null, ordered by score (descending)
-USE `hbtn_0c_0`;
-SELECT score, name
-FROM second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+-- Select score and name columns from second_table where name is not empty, ordered by score descending
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ''
+ORDER BY `score` DESC;
 
