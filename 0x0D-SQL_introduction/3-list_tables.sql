7e73bca6c837301e5f5f4dc60376233f35a27085
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:48:18 -0700

    3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,14 @@
--- Script to list all tables in a specific database
+DELIMITER //
 
--- Query to select all tables in the specified database
-USE `dbname`;
-SHOW TABLES;
+CREATE PROCEDURE list_tables_in_database(IN database_name VARCHAR(255))
+BEGIN
+    SET @query = CONCAT('USE ', database_name, ';');
+    PREPARE statement FROM @query;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
+    SHOW TABLES;
+END //
+
+DELIMITER ;
 
