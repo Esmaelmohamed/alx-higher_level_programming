6516fcadf14512cbfc3abb648b0b8f736f8970d8
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-03-14 11:53:44 -0700

    7-c

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
--- Script to insert a new row into the table first_table
-
--- Insert the new row into the first_table
-USE `hbtn_0c_0`;
-INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- Insert a new row into the specified table
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, 'Best School');
 
