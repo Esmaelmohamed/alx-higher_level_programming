5b3161ba86ffc4534b73bd3bdf587a5d68d26d71
Elab <esmael@kali>
2024-04-08 20:42:46 -0500

    concat

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the first and second arguments passed to the script
+const firstArgument = process.argv[2];
+const secondArgument = process.argv[3];
+
+// Print the arguments in the specified format
+console.log(`${firstArgument} is ${secondArgument}`);
