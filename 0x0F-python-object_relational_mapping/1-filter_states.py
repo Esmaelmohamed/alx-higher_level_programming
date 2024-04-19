72c969862d71c823224b304dc6504ea1c876c470
Elab <esmael@kali>
2024-04-19 06:47:56 -0500

    newine

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,28 @@
+#!/usr/bin/python3
+"""Lists states whose names start with 'N' from the database"""
+
 import MySQLdb
-import sys
-
-def list_states_starting_with_N(username, password, database):
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost",
-                             user=username,
-                             passwd=password,
-                             db=database,
-                             port=3306)
-        cursor = db.cursor()
-
-        # Execute SQL query to retrieve states starting with 'N'
-        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-        
-        # Fetch all rows and print results
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-        # Close cursor and database connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-        sys.exit(1)
+from sys import argv
 
 if __name__ == "__main__":
-    # Check if all three arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-    
-    # Extract arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Connect to MySQL server
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
 
-    # Call function to list states starting with 'N'
-    list_states_starting_with_N(username, password, database)
+    # Create a cursor object to interact with the database
+    cur = conn.cursor()
+
+    # Execute SQL query to retrieve states starting with 'N'
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    # Fetch all rows that start with 'N'
+    query_rows = cur.fetchall()
+    
+    # Iterate over the query results and print the states starting with 'N'
+    for row in query_rows:
+        if row[1].startswith("N"):
+            print(row)
+
+    # Close cursor and database connection
+    cur.close()
+    conn.close()
