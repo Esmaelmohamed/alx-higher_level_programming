9a0b27355fa6ba65a0656b656eea6b6394fe4ff6
Esmaelmohamed <asmaelmohamed502@gmail.com>
2024-04-23 17:27:40 -0700

    js

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/**
+ * Represents a rectangle.
+ */
+class Rectangle {
+  /**
+   * Create a rectangle.
+   * @param {number} width - The width of the rectangle.
+   * @param {number} height - The height of the rectangle.
+   */
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+module.exports = Rectangle;
+
